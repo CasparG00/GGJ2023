495165f5ea7d57e7e97f00f9bb9efbc778313629
Rettalic <ybroeks11@gmail.com>
2023-02-05 12:44:55 +0100

    Werk

--- Assets/SwitchCam.cs
+++ Assets/SwitchCam.cs
@@ -7,10 +7,38 @@ public class SwitchCam : MonoBehaviour
     public GameObject cam1;
     public GameObject cam2;
 
+    private bool EEEE;
+
+
+    
 
     private void OnTriggerEnter(Collider other)
     {
-       SwitchCamera();
+       EventSystem<WorldMessage>.InvokeEvent(EventType.onUIEnter, new WorldMessage(transform, "PRESS E TO BUILD"));
+        EEEE = true;
+    }
+
+    private void Update()
+    {
+        if (EEEE)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SwitchCamera();
+                EEEE = false;
+                Destroy(gameObject);
+                EventSystem.InvokeEvent(EventType.onUIExit);
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        EventSystem.InvokeEvent(EventType.onUIExit);
     }
 
     public void SwitchCamera()
